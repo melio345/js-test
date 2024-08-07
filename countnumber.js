7b8e5ae487d237b7a6b73fbda1058c1f9f23c8e8
melio345 <123952578+melio345@users.noreply.github.com>
2024-08-07 23:03:11 +0200

    Add files via upload

---
+++ countnumber.js
@@ -0,0 +1,53 @@
+const decrease= document.getElementById('decrease');
+const countlabel= document.getElementById('countlabel')
+const productcount=document.getElementById('productcount')
+const increase= document.getElementById('increase');
+const reset= document.getElementById('reset');
+const product1=document.getElementById("product1");
+const product2=document.getElementById("product2");
+const reset2=document.getElementById("reset2");
+const finalprice=document.getElementById("finalprice")
+let sub=document.getElementById('sub')
+
+
+
+
+let count=0;
+let product=0;
+
+increase.onclick=function(){
+    count++;
+    countlabel.textContent=count;
+}
+        decrease.onclick=function(){
+            count--;
+            countlabel.textContent=count;
+        }
+                reset.onclick=function(){
+                    count=0;
+                    countlabel.textContent=count;
+                }
+
+
+product1.onclick= function(){
+    product+=5;
+    productcount.textContent=product;
+}
+            product2.onclick= function(){
+                product-=5;
+                productcount.textContent=product;
+            }
+                    reset2.onclick= function(){
+                        product=0;
+                        productcount.textContent=product;
+                    }
+sub.onclick=function(){
+    sub=product*count
+    finalprice.textContent=(`${sub}$`);
+    if (sub==0){
+        alert('you have no money or you put nothing in the cart');
+}
+    else{
+      alert (`you have paid ${finalprice.textContent}`);
+    }
+}
